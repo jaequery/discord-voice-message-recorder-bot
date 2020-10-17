39cfab8886425035177148f910cb4b879d82004b
jae lee <jaequery@gmail.com>
2020-10-17 12:43:56 -0700

    - removed test script

--- test.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log("Bot started.");
-});
-
-client.login("NzY3MDk1ODg5NTgzMTQ0OTkx.X4s7uA.EYPhPCoMS-cqCZe3ycko0nK2OpA");
